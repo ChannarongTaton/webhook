ffd7c19aa31c2351b533c5dc829f4623c4a898f0
Tatonq <tonloveyou7@gmail.com>
2022-11-30 00:18:08 +0700

    Update cron.js
    ใช้ return ให้กับ req.method

--- router/cron.js
+++ router/cron.js
@@ -8,28 +8,30 @@ const config = {
     channelAccessToken: `${process.env.CHANNEL_ACCESS_TOKEN}`,
     channelSecret: `${process.env.CHANNEL_SECRET}`
 }
-
+var date = new Date()
+var hour = date.getHours();
+hour = (hour < 10 ? "0" : "") + hour;
+var min  = date.getMinutes();
+min = (min < 10 ? "0" : "") + min;
+var sec  = date.getSeconds();
+sec = (sec < 10 ? "0" : "") + sec;
+console.log(hour + ":" + min + ":" + sec);
 const lineClient = new line.Client(config)
 router.use((req, res) => {
     console.log(req.method);
-    console.log(req.headers);
-    setTimeout(() => {
-        console.log("ทำงานหลังจาก 5 วินาที");
-        lineClient.pushMessage(`${process.env.USER_ID_TATON}`,
+    // lineClient.pushMessage(`${process.env.USER_ID_TATON}`,
+    // {
+    //     "type": "text",
+    //     "text": `ตื่นมาแจ้งเตือนแล้ว`,
+    // })
+    if(req.method == 'GET') {
+        return lineClient.pushMessage(`${process.env.USER_ID_TATON}`,
         {
             "type": "text",
             "text": `ตื่นมาแจ้งเตือนแล้ว`,
         })
-    }, 5000);
-    // if(req.method == 'GET') {
-    //     console.log(req.method);
-    //     lineClient.pushMessage(`${process.env.USER_ID_TATON}`,
-    //     {
-    //         "type": "text",
-    //         "text": `ตื่นมาแจ้งเตือนแล้ว`,
-    //     })
-    //     // lineClient.pushMessage(`${process.env.USER_ID_BABE}`,mainFlex)
-    // }
+        // lineClient.pushMessage(`${process.env.USER_ID_BABE}`,mainFlex)
+    }
     res.json({message:"HELLO"})
 })
 
