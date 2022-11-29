8b18f4b97fcfd55b78603a77eee6be32415803c9
Tatonq <tonloveyou7@gmail.com>
2022-11-30 02:10:15 +0700

    Update Variable Fixed

--- router/cron.js
+++ router/cron.js
@@ -2,7 +2,6 @@ const express = require('express')
 const router = express.Router()
 const line = require('@line/bot-sdk')
 const mainFlex = require('../json/MainFlexSender.json')
-const moment = require('moment')
 require('dotenv').config()
 
 const config = {
@@ -28,14 +27,14 @@ console.log(currentTime);
 router.use( async (req, res) => {
     console.log(req.method);
     console.log(splitTime[0]);
-    if(req.method == 'GET' && splitTime[0] == '1') {
+    if(req.method == 'GET' && splitTime[0] == '9') {
         console.log(`จาก ${req.method}`);
         await lineClient.pushMessage(`${process.env.USER_ID_TATON}`,
         {
             "type": "text",
             "text": `ตื่นมาแจ้งเตือนแล้ว`,
         })
-        await lineClient.pushMessage(`${process.env.USER_ID_TATON}`, mainFlex)
+        await lineClient.pushMessage(`${process.env.USER_ID_BABE}`, mainFlex)
         res.json({message: `${hour}` + ":" + `${min}` + ":" + `${sec}`})
     } else {
         res.json({message:"HELLO"})
