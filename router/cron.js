959c57c320bb175282144442f515810b5dda6b82
Tatonq <tonloveyou7@gmail.com>
2022-11-30 01:28:16 +0700

    Update cron.js
    splitTime

--- router/cron.js
+++ router/cron.js
@@ -16,16 +16,15 @@ var min  = date.getMinutes();
 min = (min < 10 ? "0" : "") + min;
 var sec  = date.getSeconds();
 sec = (sec < 10 ? "0" : "") + sec;
-console.log("1. "+moment().format('LT'));
-console.log("2. "+hour + ":" + min + ":" + sec);
+// console.log("1. "+moment().format('LT'));
+// console.log("2. "+hour + ":" + min + ":" + sec);
 // console.log(date.getUTCHours()+":"+date.getUTCMinutes()+":"+date.getUTCSeconds());
 // console.log(date.toLocaleString('en-US',{ timeZone: 'Asia/Jakarta' }));
 const lineClient = new line.Client(config)
 
 var options = {
     timeZone: "Asia/Jakarta",
-    year: 'numeric', month: 'numeric', day: 'numeric',
-    hour: 'numeric', minute: 'numeric', second: 'numeric'
+    hour: 'numeric', minute: 'numeric'
 };
 
 var formatter = new Intl.DateTimeFormat([], options);
@@ -33,18 +32,25 @@ var formatter = new Intl.DateTimeFormat([], options);
 var UTCTime = date;
 var localTime = formatter.format(new Date(UTCTime));
 var currentTime = formatter.format(new Date());
-console.log("3. "+currentTime+"\n4. "+localTime);
+// console.log("3. "+currentTime+"\n4. "+localTime);
+console.log(currentTime.length);
+let splitTime = currentTime.split('', 2)
+if(splitTime[0]+splitTime[1] === '01') {
+    console.log(true);
+} else {
+    console.log(false);
+}
 
 router.use( async (req, res) => {
     console.log(req.method);
+    console.log(currentTime);
     // lineClient.pushMessage(`${process.env.USER_ID_TATON}`,
     // {
     //     "type": "text",
     //     "text": `ตื่นมาแจ้งเตือนแล้ว`,
     // })
-    if(req.method == 'GET') {
+    if(req.method == 'GET' && splitTime[0]+splitTime[1] == '01') {
         console.log(`จาก ${req.method}`);
-        console.log(hour + ":" + min + ":" + sec);
         await lineClient.pushMessage(`${process.env.USER_ID_TATON}`,
         {
             "type": "text",
