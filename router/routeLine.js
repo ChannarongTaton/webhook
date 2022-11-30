c8c64ed4588c9903497314074d881c43b570a91f
Tatonq <tonloveyou7@gmail.com>
2022-12-01 00:02:40 +0700

    Update routeLine.js
    Add Async Await

--- router/routeLine.js
+++ router/routeLine.js
@@ -17,26 +17,25 @@ const config = {
 const lineClient = new line.Client(config)
 
 router.use((req, res, next) => {
-    const event = req.body.events
+    const events = req.body.events
     console.log(event);
-    if(event.type === "message") {
-        if (event.message.type !== 'text') {
-            return
-        } else if (event.source.userId === `${process.env.USER_ID_TATON}` && event.message.text === 'กินแล้วค้าบบบ') {
-            lineClient.replyMessage(event.replyToken, sendAcceptToTaton)
-            lineClient.pushMessage(`${process.env.USER_ID_TATON}`, textAcceptPim)
-        } else if (event.source.userId === `${process.env.USER_ID_BABE}` && event.message.text === 'ยังไม่ได้กินเลย บอทกลับมาเตือนอีกรอบนะ') {
-            lineClient.replyMessage(event.replyToken, sendLaterTotaton)
-            lineClient.pushMessage(`${process.env.USER_ID_TATON}`, textLaterPim)
-        } else if (event.source.userId === `${process.env.USER_ID_BABE}` && event.message.text === 'รูป') {
-            lineClient.replyMessage(event.replyToken, randomPicture())
-        } else if (event.source.userId === `${process.env.USER_ID_TATON}` && event.message.text === 'รูป') {
-            lineClient.replyMessage(event.replyToken, randomPicture())
+    events.forEach(async event => {
+        if(event.type === "message") {
+            if (event.message.type !== 'text') {
+                return
+            } else if (event.source.userId === `${process.env.USER_ID_TATON}` && event.message.text === 'กินแล้วค้าบบบ') {
+                await lineClient.replyMessage(event.replyToken, sendAcceptToTaton)
+                await lineClient.pushMessage(`${process.env.USER_ID_TATON}`, textAcceptPim)
+            } else if (event.source.userId === `${process.env.USER_ID_BABE}` && event.message.text === 'ยังไม่ได้กินเลย บอทกลับมาเตือนอีกรอบนะ') {
+                await lineClient.replyMessage(event.replyToken, sendLaterTotaton)
+                await lineClient.pushMessage(`${process.env.USER_ID_TATON}`, textLaterPim)
+            } else if (event.source.userId === `${process.env.USER_ID_BABE}` && event.message.text === 'รูป') {
+                await lineClient.replyMessage(event.replyToken, randomPicture())
+            } else if (event.source.userId === `${process.env.USER_ID_TATON}` && event.message.text === 'รูป') {
+                await lineClient.replyMessage(event.replyToken, randomPicture())
+            }
         }
-    }
-    // events.forEach(async event => {
-
-    // });
+    });
     res.status(200).send(req.method)
 })
 
