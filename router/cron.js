22bce407f32fd7fa2df16a32cce8ef8ade6ef4cf
Tatonq <35265855+Tatonq@users.noreply.github.com>
2023-04-10 19:09:51 +0700

    เปลี่ยนแจ้งเตือนกินยา
    เปลี่ยนเวลาแจ้งเตือนเป็น 20:20

--- router/cron.js
+++ router/cron.js
@@ -25,13 +25,16 @@ var UTCTime = date;
 var localTime = formatter.format(new Date(UTCTime));
 var currentTime = formatter.format(new Date());
 var splitTimeMoment = moment().format('LT').split('', 2)
-let splitTime = currentTime.split('', 2)
+let splitTime = currentTime.split('', 6)
 router.use( async (req, res) => {
+    console.log(splitTime);
     console.log(req.method);
-    console.log("ธรรมดา", splitTime[0]+splitTime[1]);
-    console.log("โมเม้น", splitTimeMoment[0]+splitTimeMoment[1]);
-    console.log(currentTime);
-    if(req.method == 'GET' && splitTime[0] == '9') {
+    console.log(splitTime[0]+splitTime[1]+":"+splitTime[3]+splitTime[4]);
+    console.log('================');
+    // console.log("ธรรมดา", splitTime[0]+splitTime[1]+':'+splitTime[4]+splitTime[5]);
+    // console.log("โมเม้น", splitTimeMoment[0]+splitTimeMoment[1]);
+    // console.log(currentTime);
+    if(req.method == 'GET' && splitTime[0] == '2' && splitTime[1] == '0' && splitTime[3] == '2' && splitTime[4] == '0') {
         console.log(`จาก ${req.method}`);
         await lineClient.pushMessage(`${process.env.USER_ID_TATON}`,
         {
